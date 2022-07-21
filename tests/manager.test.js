85fa43a539e9cbe3ffdc4a1426d00a4932dae4bd
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-07-21 19:19:38 -0400

    managertest

---
+++ tests/manager.test.js
@@ -0,0 +1,12 @@
+const employee = require("../lib/manager")
+
+describe("manager", () => {
+    describe ("Initilization", () => {
+        it ("should be created with name, id, email and github ")
+             
+        const teamMembers = new employee ("Kothi", 3, "kothiiszulasson@kitty.com", "kothirizal")
+    })
+    const employee = "Kothi";
+    const id = 2;
+    const email = "kothiiszulasson@kitty.com"
+});
